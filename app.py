9c8b2cbe32230f351a741dabdd28b979f20f464b
Udit Anand <udianand@users.noreply.github.com>
2024-12-29 01:49:04 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pandas as pd
 from utils import get_stock_data
 import logging
 
+import finnhub
+import os 
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
@@ -17,6 +20,21 @@ tickerSymbol = st.text_input("Enter a ticker symbol eg AAPL, MSFT, TSLA", )
 start_date = st.date_input("Start Date",pd.to_datetime('2020-01-01'))
 end_date = st.date_input("End Date", pd.to_datetime("today"))
 
+# FINNHUB RECO TEST
+fiinhub_api_key  = os.environ['FINNHUB_API_KEY']
+finnhub_client = finnhub.Client(api_key = fiinhub_api_key)
+latest_recommendation = finnhub_client.recommendation_trends('AAPL')[0]
+latest_buy = latest_recommendation['buy']
+latest_hold = latest_recommendation['hold']
+latest_sell = latest_recommendation['sell']
+latest_strong_buy = latest_recommendation['strongBuy']
+latest_strong_sell = latest_recommendation['strongSell']
+latest_period = latest_recommendation['period']
+
+st.write("Recommendation")
+st.write(latest_strong_buy)
+
+print(finnhub_client.recommendation_trends('AAPL'))
 print(start_date)
 print(end_date)
 # Fetch and display data
@@ -29,10 +47,10 @@ if st.button("Get Data"):
               # Stock info header
               st.subheader(f"{stock_info.get('longName', tickerSymbol)} ({tickerSymbol})")
               st.markdown(f"**Sector:** {stock_info.get('sector', 'N/A')} | **Industry:** {stock_info.get('industry', 'N/A')}")
-              st.markdown(f"**RecommendationKey:** {stock_info.get('recommendationKey', 'N/A')} | **Currency:** {stock_info.get('currency', 'N/A')}")
+              
               #st.write(stock_info.calendar)
               #st.write(stock_info.analyst_price_targets)
-              st.write(hist_data)
+              # st.write(hist_data)
       except Exception as e:
         st.error("Error fetching data. Please check the ticker and try again.")
 
